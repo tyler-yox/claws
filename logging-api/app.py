623498aa8bcd2ad94c7596dc9cbf40a50afce165
Tyler Yox <tyleryox.dev@gmail.com>
2025-09-29 14:02:06 -0400

    added frontend data entry module

--- logging-api/app.py
+++ logging-api/app.py
@@ -1,18 +1,21 @@
-from flask import Flask, request
+import threading
 import time
-import requests
 
+import requests
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-device_ip: str = None
-label = None
+device_ip: str = '192.168.1.112'
+current_label = {}
+data_freq = {}
 
 @app.route("/data", methods=['POST'])
-def csi__data():
+def csi_data_callback():
     csi = request.json
     csi['timestamp'] = time.time()
-    csi['label'] = label
+    csi['label'] = current_label
     print(csi)
+    data_freq[str(current_label)] = data_freq.get(str(current_label), 0) + 1
     return {}
 
 
@@ -23,13 +26,29 @@ def register_device_ip():
     print(device_ip)
     return {}
 
+
 @app.route("/start", methods=['POST'])
 def start_counter():
-    global label
-    label = request.json['label']
-    print(label)
+    response = {}
+    global current_label
+    current_label = {'room': request.json['room'], 'x': request.json['x'], 'y': request.json['y']}
+
+    if data_freq.get(str(current_label), 0) < 10:
+        try:
+            threading.Thread(target=start_counter_thread).start()
+        except:
+            print('Failed to connect to device.')
+    else:
+        response['error'] = "already measured"
+    return response
+
+
+def start_counter_thread():
     requests.get('http://' + device_ip + '/start')
-    return {}
 
 
-app.run(host="0.0.0.0")
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+app.run(host="0.0.0.0", debug=True)
